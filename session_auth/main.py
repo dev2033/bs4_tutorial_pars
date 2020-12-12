7f950e44392860c3703768d3d643855e77d0c930
dev2033 <dev.elop.02@mail.ru>
2020-12-12 17:31:35 +0300

    new comm

--- session_auth/main.py
+++ session_auth/main.py
@@ -1,20 +1,41 @@
 import requests
 import time
+import fake_useragent
 
 from bs4 import BeautifulSoup
 
 from custom_logging import logger
 
+
+user = fake_useragent.UserAgent().random
+
+# создание сессии для сохранения куки, для того чтобы
+# дальше не авторизовываться во время других запросов
+session = requests.Session()
+
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                  'AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/86.0.4240.183 Safari/537.36'
+    'user-agent': user
 }
 
+data = {
+    'запрос_логин': 'логин',
+    'запрос_пароль': 'пароль'
+}
 
-def main():
-    pass
+link = "https://..."
 
+responce = session.get(link, data=data, headers=headers)
 
-if __name__ == '__main__':
-    main()
+# создание куки списка для того чтобы в дальнейшем можно было
+# его вызывать для авторизации
+cookies_dict = [
+    {"domain": key.domain, "name": key.name, "path": key.path,
+     "value": key.value}
+    for key in session.cookies
+]
+
+session2 = requests.Session()
+for cookies in cookies_dict:
+    session2.cookies.set(**cookies)
+
+resp = session2.get()
